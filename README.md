71e916417c8386f04ebf586a50f16db56a2843ea
Megham Garg <gargmegham3@gmail.com>
2024-02-24 10:56:09 +0530

    Add license and contributing guidelines

--- README.md
+++ README.md
@@ -1,7 +1,29 @@
-# Music Recommendation System based on item-to-item based collabrative filtering in python 3.7
+# Music Recommendation System based on item-to-item based collaborative filtering in Python 3.7
 
-This is a POC(proof of concept repository)
-In task2.py lies the code for recommendation system that uses item to item based collabrative filtering.
-Here i am reading sample data from https://static.turi.com/datasets/millionsong/10000.txt and https://static.turi.com/datasets/millionsong/song_data.csv.
-I am testing this code for song recommendation for a million song dataset.
-I have written a separate module named Recommenders.py for recommendation algorithm that uses co-occourence matrix and imported that module in the SongRecommender.py file.
+This repository contains a proof of concept (POC) implementation of a music recommendation system using item-to-item based collaborative filtering. The system is implemented in Python 3.7.
+
+## Overview
+The recommendation system is designed to provide song recommendations based on user preferences and similarities between songs. It utilizes the item-to-item collaborative filtering technique to identify similar songs and recommend them to users.
+
+## Files
+- `SongRecommender.py`: This file contains the code for the recommendation system. It reads sample data from two sources: [https://static.turi.com/datasets/millionsong/10000.txt](https://static.turi.com/datasets/millionsong/10000.txt) and [https://static.turi.com/datasets/millionsong/song_data.csv](https://static.turi.com/datasets/millionsong/song_data.csv). The code is tested on a dataset of a million songs.
+- `Recommenders.py`: This module contains the recommendation algorithm that uses a co-occurrence matrix. It is imported and used in the `SongRecommender.py` file.
+
+## Usage
+To use the music recommendation system, follow these steps:
+1. Clone the repository: `git clone https://github.com/gargmegham/MusicRecommendationEngine.git`
+2. Install the required dependencies: `pip install -r requirements.txt`
+3. Run the `SongRecommender.py` file: `python SongRecommender.py`
+
+## Contributing
+
+Contributions are welcome! If you have any ideas, suggestions, or bug reports, please open an issue or submit a pull request. Make sure to follow the [contribution guidelines](CONTRIBUTING.md).
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
+
+
+## Contact
+
+For any questions or inquiries, please contact [meghamgarg@gmail.com](mailto:meghamgarg@gmail.com).
--- SongRecommender.py
+++ SongRecommender.py
@@ -1,35 +1,43 @@
 from sklearn.model_selection import train_test_split
-import numpy as np
 import pandas
 import Recommenders as Recommenders
-
-if __name__ == "__main__" :
-    
-    df1 = pandas.read_csv('https://static.turi.com/datasets/millionsong/10000.txt',header=None,sep='\t')
+ 
+def load_data():
+    """
+    Load and preprocess data.
+    """
+    df1 = pandas.read_csv('https://static.turi.com/datasets/millionsong/10000.txt', header=None, sep='\t')
     df1.columns = ['user_id', 'song_id', 'listen_count']
-    df2 =  pandas.read_csv('https://static.turi.com/datasets/millionsong/song_data.csv')
+    df2 = pandas.read_csv('https://static.turi.com/datasets/millionsong/song_data.csv')
     df = pandas.merge(df1, df2.drop_duplicates(['song_id']), on="song_id", how="left")
     
-    #decrease the dataframe length to 10000 just to test the data
-    df=df.head(10000)
+    # Decrease the dataframe length to 10000 just to test the data
+    df = df.head(10000)
     
-    #Merge song title and artist_name columns to make a merged column
+    # Merge song title and artist_name columns to make a merged column
     df['song'] = df['title'].map(str) + " - " + df['artist_name']
+    return df
+
+def main():
+    """
+    Main function to demonstrate recommendation process.
+    """
+    df = load_data()
 
     song_grouped = df.groupby(['song']).agg({'listen_count': 'count'}).reset_index()
     grouped_sum = song_grouped['listen_count'].sum()
-    song_grouped['percentage']  = song_grouped['listen_count'].div(grouped_sum)*100
-    most_popular_songs = song_grouped.sort_values(['listen_count', 'song'], ascending = [0,1])
+    song_grouped['percentage'] = song_grouped['listen_count'].div(grouped_sum) * 100
+    most_popular_songs = song_grouped.sort_values(['listen_count', 'song'], ascending=[0, 1])
 
     users = df['user_id'].unique()
     songs = df['song'].unique()
 
-    train_data,test_data = train_test_split(df, test_size = 0.2, random_state=0)
+    train_data, test_data = train_test_split(df, test_size=0.2, random_state=0)
 
     is_model = Recommenders.item_similarity_recommender_py()
     is_model.create(train_data, 'user_id', 'song')
 
-    #Print the songs for the user in training data
+    # Echo the songs for the user in training data
     user_id = users[5]
     user_items = is_model.get_user_items(user_id)
 
@@ -37,6 +45,9 @@ if __name__ == "__main__" :
     for user_item in user_items:
         print(user_item)
     print("****************Recommendation process going on****************")
-    #Top 10 Recommend songs for the user using personalized model
+    # Top 10 Recommended songs for the user using personalized model
     recommended = is_model.recommend(user_id)
-    print(recommended[['song','score','rank']])
+    print(recommended[['song', 'score', 'rank']])
+
+if __name__ == "__main__":
+    main()
